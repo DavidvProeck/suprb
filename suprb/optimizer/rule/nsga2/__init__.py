1e38c33a0ec97d6a0ad139ed9898ef26333505ae
vonproda <david.von.proeck-zvlcil@uni-a.de>
2025-09-17 14:56:58 +0200

    change init for nsga2

--- suprb/optimizer/rule/nsga2/__init__.py
+++ suprb/optimizer/rule/nsga2/__init__.py
@@ -1,6 +1,7 @@
 from .nsga2 import NSGA2
-from src.suprb.suprb.optimizer.rule.nsga2.pymoo.pymoo_nsga2 import PymooNSGA2
+from .pymoo.pymoo_nsga2 import PymooNSGA2
 from .nsga2_global_novelty import NSGA2GlobalNovelty
 from .nsga2_novelty_G_P import NSGA2Novelty_G_P
+from .nsga2ig import NSGA2InfoGain
 #Do not know if needed
 from .nsga2_helpers import visualize_pareto_front
